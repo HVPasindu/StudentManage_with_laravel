b2178aab4977c03fad8386dfe7a4d782af648f3c
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-07-28 18:39:10 +0530

    Restore StudentController with index method to retrieve and display students

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,15 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
-use Illuminate\Http\Request;
-
-class StudentController extends Controller
+abstract class Controller
 {
-    public function index()
-    {
-        $students = Student::latest()->get();
-
-        return view('students.index', compact('students'));
-    }
+    //
 }
