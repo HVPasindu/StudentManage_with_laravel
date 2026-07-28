13e221312f6c7a4f5dce2a31f0acfa95432f04e0
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-07-28 18:47:25 +0530

    Add index view for displaying students

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+</head>
+<body>
+
+    <h1>Students</h1>
+
+    <table border="1" cellpadding="10">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Student Number</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Date of Birth</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->student_number }}</td>
+                    <td>{{ $student->first_name }}</td>
+                    <td>{{ $student->last_name }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td>{{ $student->date_of_birth }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
+</html>
