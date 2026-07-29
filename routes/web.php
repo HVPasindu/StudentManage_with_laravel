af31621b8904e902f1b95cac701c322ae7cb0466
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-07-29 13:23:32 +0530

    Refactor student routes to use resource controller and comment out individual routes

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,36 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-Route::get('/students', [StudentController::class, 'index'])
-    ->name('students.index');
+// Route::get('/students', [StudentController::class, 'index'])
+//     ->name('students.index');
 
 
-Route::get('/students/create', [StudentController::class, 'create'])
-    ->name('students.create');
+// Route::get('/students/create', [StudentController::class, 'create'])
+//     ->name('students.create');
 
 
-Route::post('/students', [StudentController::class, 'store'])
-    ->name('students.store');
+// Route::post('/students', [StudentController::class, 'store'])
+//     ->name('students.store');
 
 
-Route::get('/students/{student}', [StudentController::class, 'show'])
-    ->name('students.show');
+// Route::get('/students/{student}', [StudentController::class, 'show'])
+//     ->name('students.show');
 
 
-Route::get('/students/{student}/edit', [StudentController::class, 'edit'])
-    ->name('students.edit');
+// Route::get('/students/{student}/edit', [StudentController::class, 'edit'])
+//     ->name('students.edit');
 
 
-Route::put('/students/{student}', [StudentController::class, 'update'])
-    ->name('students.update');
+// Route::put('/students/{student}', [StudentController::class, 'update'])
+//     ->name('students.update');
 
 
 
-Route::delete('/students/{student}', [StudentController::class, 'destroy'])
-    ->name('students.destroy');
+// Route::delete('/students/{student}', [StudentController::class, 'destroy'])
+//     ->name('students.destroy');
+
+
+// me 7 ma karanna ekak thiye eka me 7 ma karano but controller eketh ara gaththa function name ma ganna oni
+// naththan laravel valata hoya ganna bari veno
+
+Route::resource('students', StudentController::class);
